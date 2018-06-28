2572b9724b5f0e2877d31e8790f19c70e22224fc
DanialDaeHyunNam <danialnam89@gmail.com>
2018-06-28 11:14:45 +0900

    Making progress

--- modeling_danial/costum_functions.py
+++ modeling_danial/costum_functions.py
@@ -1,8 +1,8 @@
 
 """
     포함된 함수 정보
-    
-    
+
+
     dateToNumeric : date컬럼을 2014-01-01의 object type에서 20140101의 int type으로 변환하는 함수.
     getStore : 원하는 지점의 정보만을 dataframe type으로 리턴한다.
     getItemInfo : 원하는 지점의 원하는 아이템의 판매정보만을 df type으로 리턴한다.
@@ -11,7 +11,7 @@
     isThereNoneData : Missing data, Trace data의 개수정보를 받아서 만든 dataframe을 리턴한다.
     checkIsThereUnderZeroData : df의 컬럼마다 0이하에 해당하는 데이터가 몇개있는지 리턴한다.
     classifyStoresByStation : key데이터프레임을 넣어주게되면 같은 기상청을 공유하는 지점들을 따로 분할한 데이터프레임을 리턴한다.
-    df_train_specified_store : 이 함수는 train df와 classifyStoresByStation를 통해 얻은 기상청마다 
+    df_train_specified_store : 이 함수는 train df와 classifyStoresByStation를 통해 얻은 기상청마다
             관할하는 지점들의 정보를 담은 df를 패러미터로 넣어주고, 원하는 기상청 번호를 parameter로 넣어주면 거기에 해당하는 지점들의 정보만을 리턴한다.
     divideIntoNumericAndCategoricalVariables : 컬럼을 확인해서 numerical_features와 categorical_features을 나누는 작업을 해준다.
     saveDataFrameToCsv : 넘겨준 df를 filename + 년월일시간분 의 format으로 이루어진 이름의 파일로 생성해준다.
@@ -99,13 +99,13 @@ def isThereNoneData(df):
 def checkIsThereUnderZeroData(df, df_columns, dtype, num_to_compare_with = 0):
     """
         df의 컬럼마다 0이하에 해당하는 데이터가 몇개있는지 리턴한다.
-        용도는 M, T데이터를 우리가 어떻게 수치화할것인지 결정할 때 기준을 잡는 것이다. 
-        num_to_compare_with의 default값은 0이지만, 
+        용도는 M, T데이터를 우리가 어떻게 수치화할것인지 결정할 때 기준을 잡는 것이다.
+        num_to_compare_with의 default값은 0이지만,
         num_to_compare_with의 초기값을 바꾸며 하나도 존재하지 않는 데이터를 찾아서
         M, T를 대신할 숫자를 결정하기위하여 사용하는 함수.
-        
+
         패러미터로는 df, df_columns, dtype(str type으로 넣어준다. 예:) "int"), num_to_compare_with
-        
+
         df_columns에는
             coulmns_should_be_integer_type_of_data을 넣거나
             coulmns_should_be_float_type_of_data을 넣어서 사용한다.
@@ -117,7 +117,7 @@ def checkIsThereUnderZeroData(df, df_columns, dtype, num_to_compare_with = 0):
         for i, contents in enumerate(df[l]):
             contents = str(contents)
             if(contents.strip() != "M" and contents.strip() != "T"):
-                if(dtype == "int"):                    
+                if(dtype == "int"):
                     if(int(contents) < num_to_compare_with):
                         count += 1
                 if(dtype == "float"):
@@ -138,7 +138,7 @@ def classifyStoresByStation(df):
             dictionary[station_nbr] += ", " + store_nbr
         else:
             dictionary[station_nbr] = store_nbr
-    
+
     return pd.DataFrame({"station_nbr" : list(dictionary.keys()), "store_nbr" : list(dictionary.values())})
 
 def df_train_specified_store(df, store_nbrs, station_nbr):
@@ -176,11 +176,11 @@ def divideIntoNumericAndCategoricalVariables(df):
     print("Numerical Features : ", numerical_features, "\n\n")
     print("Categorical Features Qty :", len(categorical_features),"\n")
     print("Categorical Features :", categorical_features)
-    return numerical_features, categorical_features        
+    return numerical_features, categorical_features
 
 def changeDataType(df, integer_data, float_data):
     """
-        Parameter : 
+        Parameter :
             weather df
     """
     for li in integer_data:
